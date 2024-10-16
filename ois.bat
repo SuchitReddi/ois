593730a92aa692f84af16cef80c8c54b4d25c0e6
5herl0ck <72243194+SuchitReddi@users.noreply.github.com>
2024-10-16 22:44:37 +0530

    Added multiple IOC capability
    - Added multiple IOC capability (limited at 3 to avoid resource consumption).
    - Removed the end function to not ask for confirmation to restart and go to start every time. User can exit using Ctrl+C.
    - Added hash lookup capability.
    - Printing URLs in the terminal to make copying links easier.

--- ois.bat
+++ ois.bat
@@ -2,7 +2,7 @@
 
 REM OIS - OSINT IOC Scanner
 REM Author: Suchit Reddi
-REM Opens given IP/url/domain in commonly used lookup sites.
+REM Opens given IP/url/domain/hash in commonly used lookup sites.
 
 echo   /$$$$$$          /$$           /$$                            
 echo  /$$__  $$        ^|__/          ^| $$                            
@@ -29,108 +29,134 @@ echo ^|  $$$$$$^|  $$$$$$^|  $$$$$$^| $$  ^| $^| $$  ^| $^|  $$$$$$^| $$
 echo  \______/ \_______/\_______^|__/  ^|__^|__/  ^|__/\_______^|__/     
 
 echo By Suchit Reddi
-
+echo.
 REM -------------------Script Start-----------------------------------------------
 
-:start
-
 REM <----All round Lookup---->
+set vt=https://www.virustotal.com/gui/search
 set ibm=https://exchange.xforce.ibmcloud.com
 set talos=https://talosintelligence.com/reputation_center/lookup?search
+set talos_h=https://talosintelligence.com/talos_file_reputation?s
+set kasper=https://opentip.kaspersky.com
+set otx=https://otx.alienvault.com/browse/global/pulses?q
+
+REM <----Domain, IP, URL---->
 set shodan=https://www.shodan.io/search?query
 
-REM <----Domain & IP Lookup---->
-set vt=https://www.virustotal.com/gui/search
-set whois=https://www.whois.com/whois
-
-REM <----Just Domain Lookup---->
+REM <----Domain, URL---->
 set norton=https://sitereview.bluecoat.com/#/lookup-result
-set urlvoid=https://urlvoid.com/scan
 set ggl=https://transparencyreport.google.com/safe-browsing/search?url
-set urlscan=https://urlscan.com/domain
 
-REM <----Just IP Lookup---->
+REM <----Domain, IP---->
 set abip=https://www.abuseipdb.com/check
+set whois=https://www.whois.com/whois
+set urlscan=https://urlscan.io/domain
 
-echo.
-echo Type of Lookup:
+REM <----Just Domain---->
+set urlvoid=https://urlvoid.com/scan
+
+:start
+setlocal enabledelayedexpansion
+
 echo [1] Domain
 echo [2] IP
 echo [3] URL
+echo [4] Hash
 set /p lookup="Select respective number: "
 
 if "%lookup%"=="1" (
-    call :domain
+    call :lookup_handler domain
 ) else if "%lookup%"=="2" (
-    call :ip
+    call :lookup_handler ip
 ) else if "%lookup%"=="3" (
-    call :url
+    call :lookup_handler url
+) else if "%lookup%"=="4" (
+    call :lookup_handler hash
 ) else (
-    echo Invalid number! Select from 1, 2, 3... && echo. && goto start
+    echo Invalid number! Select from 1 to 4... && echo. && goto start
 )
+goto start
 
-REM ------------------Domain Lookup-----------------------------------------------
-:domain
-echo.
-set /p dom="Enter domain (ex:- suchitreddi.github.io): "
+:lookup_handler
+REM Argument %1 will be used to distinguish between domain, ip, url, or hash
+set type=%1
+set /p ioc="Enter IOCs (max 3): "
+echo. 
 
-start msedge -new-window "%vt%/%dom%" "%urlscan%/%dom%" "%norton%/%dom%" "%ggl%=%dom%" "%whois%/%dom%" "%talos%=%dom%" "%ibm%/url/%dom%" "%urlvoid%/%dom%" "%shodan%=%dom%"
-echo. && echo Select and copy links using Ctrl+Shift+V && echo.
+REM Replace commas, OR, or with spaces to normalize the input
+set ioc=%ioc:,= % && set ioc=%ioc: OR = % && set ioc=%ioc: or = %
+set count=0
 
-echo ------------------------------------------------------------------
-echo.
-echo %vt%/%dom%  && echo %urlscan%/%dom%  && echo %norton%/%dom%  && echo %ggl%=%dom%  && echo %whois%/%dom%  && echo %talos%=%dom%  && echo %ibm%/url/%dom%  && echo %urlvoid%/%dom%  && echo %shodan%=%dom%
+REM Loop through each IOC in the user input
+FOR %%i IN (%ioc%) DO (
+    REM Increment the counter
+    set /A count+=1
 
-echo.
-echo ------------------------------------------------------------------
-echo.
-goto end
+    if !count! leq 3 (
+        if !type!==domain (
+            REM ------------------Domain Lookup------------------------------------
+            set "domains=!vt!/%%i !otx!=%%i !kasper!/%%i/?tab=lookup !urlscan!/%%i !norton!/%%i !ggl!=%%i !whois!/%%i !talos!=%%i !ibm!/url/%%i !abip!/%%i !urlvoid!/%%i !shodan!=%%i"
 
-REM ----------------------IP Lookup-----------------------------------------------
-:ip
-echo.
-set /p ip_addr="Enter IP address (ex:- 8.8.8.8): "
+            start msedge -new-window !domains!
 
-start msedge -new-window "%vt%/%ip_addr%" "%talos%=%ip_addr%" "%ibm%/ip/%ip_addr%" "%whois%/%ip_addr%" "%abip%/%ip_addr%" "%shodan%=%ip_addr%"
-echo. && echo Select and copy links using Ctrl+Shift+V && echo.
+            REM Display the URLs for the user to copy
+            echo IOC: %%i
+            echo ------------------------------------------------------------------
+            echo %vt%/%%i && echo %otx%=%%i && echo %kasper%/%%i/?tab=lookup && echo %urlscan%/%%i && echo %norton%/%%i && echo %ggl%=%%i && echo %whois%/%%i && echo %talos%=%%i && echo %ibm%/url/%%i && echo %abip%/%%i && echo %urlvoid%/%%i && echo %shodan%=%%i
+            echo ------------------------------------------------------------------
+            echo.
+        ) else if !type!==ip (
+            REM ----------------------IP Lookup------------------------------------
+            set "ips=!vt!/%%i !otx!=%%i !kasper!/%%i/?tab=lookup !urlscan!/%%i !norton!/%%i !ggl!=%%i !whois!/%%i !talos!=%%i !ibm!/url/%%i !abip!/%%i !shodan!=%%i"
 
-echo ------------------------------------------------------------------
-echo.
-echo %vt%/%ip_addr% && echo %whois%/%ip_addr% && echo %ibm%/url/%ip_addr%  && echo %shodan%=%ip_addr% && echo %talos%=%ip_addr% && echo %abip%/%ip_addr%
+            start msedge -new-window !ips!
 
-echo.
-echo ------------------------------------------------------------------
-echo.
-goto end
+            echo IOC: %%i
+            echo ------------------------------------------------------------------
+            echo %vt%/%%i && echo %otx%=%%i && echo %kasper%/%%i/?tab=lookup && echo %urlscan%/%%i && echo %norton%/%%i && echo %ggl%=%%i && echo %whois%/%%i && echo %talos%=%%i && echo %ibm%/url/%%i && echo %abip%/%%i && echo %shodan%=%%i
+            echo ------------------------------------------------------------------
+            echo.
+        ) else if !type!==url (
+            REM ----------------------URL Lookup-----------------------------------
+            call :EncodeURL %%i
+            set "urls=%vt%/!enc_url_2! %otx%=!enc_url! %kasper%/!enc_url!/?tab=lookup %norton%/!enc_url_2! %ggl%=!enc_url! %ibm%/url/!enc_url! %shodan%=!enc_url! %talos%=!enc_url!"
 
-REM ----------------------URL Lookup-----------------------------------------------
-:url
-echo.
-set /p url_addr="Enter URL (ex:- https://suchitreddi.github.io/Work/scripts.html): "
-call :EncodeURL "%url_addr%"
+            start msedge -new-window !urls!
 
-start msedge -new-window "%vt%/%enc_url_2%" "%ggl%=%enc_url%" "%ibm%/url/%enc_url%" "%shodan%=%enc_url%" "%talos%=%enc_url%"
-echo. && echo Select and copy links using Ctrl+Shift+V && echo.
+            echo IOC: %%i
+            echo ------------------------------------------------------------------
+            echo %vt%/!enc_url_2! && echo %otx%=!enc_url! && echo %kasper%/!enc_url!/?tab=lookup && echo %norton%/!enc_url_2! && echo %ggl%=!enc_url! && echo %ibm%/url/!enc_url! && echo %shodan%=!enc_url! && echo %talos%=!enc_url!
+            echo ------------------------------------------------------------------
+            echo.
+        ) else if !type!==hash (
+            REM ----------------------Hash Lookup-----------------------------------
+            set "hashes=!vt!/%%i !otx!=%%i !kasper!/%%i/results?tab=lookup !ibm!/malware/%%i !talos_h!=%%i"
 
-echo ------------------------------------------------------------------
-echo.
-echo %vt%/%enc_url_2% && echo %ggl%=%enc_url% && echo %ibm%/url/%enc_url%  && echo %shodan%=%enc_url% && echo %talos%=%enc_url%
+            start msedge -new-window !hashes!
 
-echo.
-echo ------------------------------------------------------------------
-echo.
-goto end
+            echo IOC: %%i
+            echo ------------------------------------------------------------------
+            echo %vt%/%%i && echo %otx%=%%i && echo %kasper%/%%i/results?tab=lookup && echo %ibm%/malware/%%i && echo %talos_h%=%%i
+            echo ------------------------------------------------------------------
+            echo.
+        )
+    ) else (
+        echo Sky is the limit... but 3 is the limit for now && echo.
+    )
+)
+goto start
+REM -------------------Script End---------------------------------------
 
-REM -------------------URL Encoding Function--------------------------------------
+REM -------------------URL Encoding Function----------------------------
 :EncodeURL
-setlocal EnableDelayedExpansion
+setlocal enabledelayedexpansion
 set "url=%~1"
 set "enc_url="
 set "enc_url_2="
 
 REM Loop through the URL characters for single encoding
-for /L %%i in (0,1,255) do (
-    set "chr=!url:~%%i,1!"
+for /L %%j in (0,1,255) do (
+    set "chr=!url:~%%j,1!"
     if "!chr!"=="" goto :breakLoop
 
     REM Encode special characters
@@ -162,21 +188,7 @@ for /L %%i in (0,1,255) do (
 )
 
 :breakLoop
-REM Perform the second pass (double encoding)
 REM Double encode by replacing % with %25 in enc_url
 set "enc_url_2=!enc_url:%%=%%25!"
-
 endlocal & set "enc_url=%enc_url%" & set "enc_url_2=%enc_url_2%"
 goto :eof
-
-:end
-set /p another="Do you want to search for another IOC? (y/n) "
-echo.
-if /i %another%==y (
-    goto start
-) else if /i %another%==n (
-    echo Happy Hunting! Press any key to exit... && pause>nul && exit
-) else (
-    echo Invalid option! Select y or n... && echo. && goto end
-)
-REM -------------------Script End---------------------------------------
